1899a4a1efc9c01c1657021dae75ff7c5036d305
evan-schott <53463459+evan-schott@users.noreply.github.com>
2023-04-21 20:46:36 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# stratego.aleo
+
+## Build Guide
+
+To compile this Aleo program, run:
+```bash
+aleo build
+```
---
+++ init_board.py
@@ -0,0 +1,45 @@
+A = [
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [1, 2, 2, 2, 3, 4],
+    [4, 5, 5, 6, 6, 7]
+]
+
+S = [
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0]
+]
+
+CS = 0
+
+for i in range(len(A)):
+    for j in range(len(A[0])):
+        val = (2 ** (6*i+j))*A[i][j]
+        S[i][j] = val
+        CS += val
+
+for i in range(len(S)):
+    print(S[i])
+
+print(CS)
+
+
+# def create_cumulative_sum_array(A):
+#     S = [[0 for _ in range(len(A[0]))] for _ in range(len(A))]
+
+#     for j in reversed(range(len(A))):
+#         for i in reversed(range(len(A[0]))):
+#             S[len(A)-1-j][len(A[0])-1-i] = 2**(6*j + i) * A[j][i]
+
+#     return S
+
+# S = create_cumulative_sum_array(A)
+
+# for row in S:
+#     print(row)
